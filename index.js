a774d980d3a967d26a555768aca460aefbcd4aee
Leotard Niyonkuru <leotard.n@gmail.com>
2022-08-03 21:12:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!!');
 });
 
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
